27308ddf45784bd2bcc79c1993fe838291475718
Edmond Soun <edmond.soun@gmail.com>
2022-08-18 15:09:32 -0700

    last three routes and code review notes

--- db.js
+++ db.js
@@ -3,8 +3,14 @@
 
 const { Client } = require("pg");
 
+/** MAC EDITION */
+// const DB_URI = process.env.NODE_ENV === "test"
+//   ? "postgresql:///biztime_test" : "postgresql:///biztime";
+
+
+/** WINDOWS EDITION */
 const DB_URI = process.env.NODE_ENV === "test"
-  ? "postgresql:///biztime_test" : "postgresql:///biztime";
+  ? "postgresql://esoun:esoun@localhost/biztime_test" : "postgresql://esoun:esoun@localhost/biztime";
 
 let db = new Client({ connectionString: DB_URI }); db.connect();
 
--- routes/companies.js
+++ routes/companies.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require('../db');
-const { NotFoundError } = require('../expressError');
+const { NotFoundError, BadRequestError } = require('../expressError');
 
 /** returns JSON object of all companies as {companies: [{code, name}, ...]} */
 router.get('/', async function (req, res) {
@@ -36,32 +36,82 @@ router.get('/:code', async function (req, res, next) {
   return res.status(200).json({ company: results.rows[0] });
 });
 
-// If the company given cannot be found, this should return a 404 status response.
-
-// POST /companies
-// Adds a company.
-
-// Needs to be given JSON like: {code, name, description}
-
-// Returns obj of new company: {company: {code, name, description}}
-
-// PUT /companies/[code]
-// Edit existing company.
-
-// Should return 404 if company cannot be found.
-
-// Needs to be given JSON like: {name, description}
-
-// Returns update company object: {company: {code, name, description}}
-
-// DELETE /companies/[code]
-// Deletes company.
-
-// Should return 404 if company cannot be found.
-
-// Returns {status: "deleted"}
 
 
+
+/** Add a new company. 
+ * - accepts JSON: {code, name, description}
+ * - returns  {company: {code, name, description}} or 404
+ */
+router.post('/', async function (req, res, next) {
+  const { code, name, description } = req.body;
+
+  // wrap in try/catch:
+  
+    const result = await db.query(
+      `INSERT INTO companies (code, name, description)
+        VALUES ($1, $2, $3)
+        RETURNING code, name, description`,
+        [code, name, description]
+    );
+
+ 
+  // throw new BadRequestError();
+  
+
+  const company = result.rows[0];
+  return res.status(201).json({ company })
+
+});
+
+
+
+/** Edit a company. 
+ * - accepts JSON: {name, description}
+ * - returns updated company object: {company: {code, name, description}} or 404
+ */
+
+router.put('/:code', async function (req, res, next) {
+  const { name, description } = req.body;
+
+  const result = await db.query(
+    `UPDATE companies
+      SET name = $1,
+          description = $2
+          WHERE code = $3
+      RETURNING code, name, description`,
+      [name, description, req.params.code]
+  );
+
+  if (result.rows.length === 0) {
+    throw new NotFoundError();
+  }
+
+  const company = result.rows[0];
+  return res.status(200).json({ company })
+
+});
+
+/** Delete a company. 
+ * - Returns {status: "deleted"} or 404
+ */
+
+router.delete('/:code', async function (req, res, next) {
+  
+  const result = await db.query(`DELETE FROM companies 
+      WHERE code = $1
+      RETURNING code, name, description`,
+      [req.params.code]
+  );
+
+  // assign rows to a variable and test if falsy
+  if (result.rows.length === 0) {
+    throw new NotFoundError();
+  }
+
+  return res.status(200).json({status: "deleted"})
+
+});
 
 
 module.exports = router;
